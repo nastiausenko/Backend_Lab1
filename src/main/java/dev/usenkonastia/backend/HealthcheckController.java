46355a838f807df99c999a8315f38ea98084df86
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-10-01 12:43:24 +0300

    fix healthcheck endpoint

--- src/main/java/dev/usenkonastia/backend/HealthcheckController.java
+++ src/main/java/dev/usenkonastia/backend/HealthcheckController.java
@@ -4,7 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,8 +16,13 @@ public class HealthcheckController {
     @GetMapping("/healthcheck")
     public ResponseEntity<Map<String, Object>> healthcheck() {
         Map<String, Object> response = new HashMap<>();
+        ZoneId kyivZone = ZoneId.of("Europe/Kiev");
+        ZonedDateTime now = ZonedDateTime.now(kyivZone);
+
+        String formattedDate = now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
         response.put("status", "Service is running");
-        response.put("timestamp", LocalDateTime.now().toString());
+        response.put("timestamp", formattedDate);
         return ResponseEntity.ok(response);
     }
 }
